fafbf2729de9f87cb6d7c92701fd6717bbf66bb8
James Tang <jytang10@gmail.com>
2019-10-13 11:56:09 -0700

    Added db, albums, stylesheet

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Heading() {
   return (
     <div>
-      
+      <h3>Photo App</h3>
+      <form>
+        <input type="text" placeholder="Search..." value="search"/>
+      </form>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from './components/App';
+import Heading from './components/Heading';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Heading></Heading>
+  </BrowserRouter>
+  , document.getElementById('root'));
