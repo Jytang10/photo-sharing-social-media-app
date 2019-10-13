c57d689516c7d21a0e78f6f498d9069f20873eed
James Tang <jytang10@gmail.com>
2019-10-13 13:04:34 -0700

    Mapped through people data object

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 
 class PeopleList extends Component {
 
+  renderPeople = () => {
+    if(this.props.people !== undefined){
+      return this.props.people.map(person => {
+        return (
+          <div key={person.id}>
+            <img src={`Albums/${person.name}/${person.profile_pic}.jpeg`} alt="profile pic"/>
+            <div>
+              <h1>{person.name}</h1>
+            </div>
+          </div>
+        )
+      })
+    }
+  }
+
   render() {
     console.log('peoplelist.js', this.props.people)
     return (
       <div style={{color:'white'}}>
-        <p>I am from PeopleList</p>
+        {this.renderPeople()}
       </div>
     )
   }
