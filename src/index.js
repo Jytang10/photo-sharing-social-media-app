a891b7e736e88e0e00bcae49864fa970f2d32b6e
James Tang <jytang10@gmail.com>
2019-10-13 12:36:24 -0700

    Added Provider and Middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from './components/App';
 import Heading from './components/Heading';
+import rootReducers from './reducers';
+import {Provider} from 'react-redux';
+
+import {applyMiddleware, createStore} from 'redux';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Heading></Heading>
-    <Switch>
-      <Route exact path='/' component={App}/>
-    </Switch>
-  </BrowserRouter>
+  <Provider store={createStoreWithMiddleware(rootReducers)}>
+    <BrowserRouter>
+      <Heading></Heading>
+      <Switch>
+        <Route exact path='/' component={App}/>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
